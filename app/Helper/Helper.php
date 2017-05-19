6b4320d21f127b53d3babfd3150950e2554ab125
huynt <huynt57@gmail.com>
2017-05-19 12:04:13 +0700

    rollback

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -429,9 +429,7 @@ function get_payment_success_message($notification, $locale = "vi")
     $message = '';
     if ($notification) {
         $message = str_replace("{{payment_success}}", "", $notification["content_" . $locale]);
-        if($notification->order) {
-            $message = str_replace("{{order_name}}", $notification->order->name, $message);
-        }
+        $message = str_replace("{{order_name}}", $notification->order->name, $message);
         $message = str_replace("{{traveler}}", trim($notification->from_user->first_name . " " . $notification->from_user->last_name), $message);
     }
 
