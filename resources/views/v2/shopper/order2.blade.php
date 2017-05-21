f8caabd4bc6f7c7d8f7657618a4beea265e5c933
huy <huynt57@gmail.com>
2017-05-21 22:31:05 +0700

    fix ui

--- resources/views/v2/shopper/order2.blade.php
+++ resources/views/v2/shopper/order2.blade.php
@@ -8,7 +8,7 @@
             <nav>
                 <ol class="cd-multi-steps text-bottom count">
                     <li class="visited"><a href="#0">{{trans("index.thongtinsanpham")}}</a></li>
-                    <li class="current">{{trans("index.chitietgiaohang")}}</li>
+                    <li class="current"><em>{{trans("index.chitietgiaohang")}}</em></li>
                     <li><em>{{trans("index.taoyeucau")}}</em></li>
 
                 </ol>
@@ -113,15 +113,10 @@
     @endsection
 
 @section('frontend_script')
-    {{Html::script('assets/global/plugins/cubeportfolio/js/jquery.cubeportfolio.min.js')}}
+
     {{Html::script('assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js')}}
 
-    {{Html::script('assets/pages/scripts/portfolio-1.min.js')}}
-    {{Html::script('assets/pages/scripts/convert-image-to-base64.js')}}
-    {{Html::script('assets/global/plugins/icheck/icheck.min.js')}}
-    {{Html::script('assets/global/plugins/select2/js/select2.full.min.js')}}
 
-    {{Html::script('assets/pages/scripts/validator.min.js')}}
 
     <script type="text/javascript">
         $('.datepicker').datepicker({
@@ -130,175 +125,6 @@
             autoclose: true
         });
         $('.datepicker').datepicker('setStartDate', new Date());
-        $("#updateInfoModal").modal();
-        $(".select2-single").select2();
-        $("#deliverTo").change(function () {
-            if ($(this).val() == "") {
-                $("#deliverFrom").hide();
-            } else {
-                if ($(this).val().length >= 3) {
-                    console.log("123");
-                    $("#deliverFrom").show();
-                }
-            }
-        });
-        $(".reward").click(function () {
-            $(".reward").addClass("btn-outline");
-            $(this).removeClass("btn-outline");
-            $(".input-reward").val($(this).data("reward"));
-        });
-        $(".input-reward").change(function () {
-            var rewardVal = $(this).val();
-
-            if (parseFloat(rewardVal) < 5) {
-                $(this).val("5");
-            }
-
-            $(".reward").addClass("btn-outline");
-            $(".reward").each(function () {
-                if (rewardVal == $(this).data("reward")) {
-                    $(this).removeClass("btn-outline");
-                }
-            })
-        })
-
-    </script>
-    <script type="text/javascript">
-        var ComponentsSelect2 = function () {
-
-            var handleDemo = function () {
-
-                // Set the "bootstrap" theme as the default theme for all Select2
-                // widgets.
-                //
-                // @see https://github.com/select2/select2/issues/2927
-                $.fn.select2.defaults.set("theme", "bootstrap");
-
-                var placeholder = "Select a State";
-
-                $(".select2, .select2-multiple").select2({
-                    placeholder: "{!! trans("index.diemxuatphat_select") !!}",
-                    width: null
-                });
-                $(".select21").select2({
-                    placeholder: "{!! trans("index.diemden_select") !!}",
-                    width: null
-                });
-
-                $(".select2-allow-clear").select2({
-                    allowClear: true,
-                    placeholder: placeholder,
-                    width: null
-                });
-
-                // @see https://select2.github.io/examples.html#data-ajax
-                function formatRepo(repo) {
-                    if (repo.loading) return repo.text;
-
-                    var markup = "<div class='select2-result-repository clearfix'>" +
-                        "<div class='select2-result-repository__avatar'><img src='" + repo.owner.avatar_url + "' /></div>" +
-                        "<div class='select2-result-repository__meta'>" +
-                        "<div class='select2-result-repository__title'>" + repo.full_name + "</div>";
-
-                    if (repo.description) {
-                        markup += "<div class='select2-result-repository__description'>" + repo.description + "</div>";
-                    }
-
-                    markup += "<div class='select2-result-repository__statistics'>" +
-                        "<div class='select2-result-repository__forks'><span class='glyphicon glyphicon-flash'></span> " + repo.forks_count + " Forks</div>" +
-                        "<div class='select2-result-repository__stargazers'><span class='glyphicon glyphicon-star'></span> " + repo.stargazers_count + " Stars</div>" +
-                        "<div class='select2-result-repository__watchers'><span class='glyphicon glyphicon-eye-open'></span> " + repo.watchers_count + " Watchers</div>" +
-                        "</div>" +
-                        "</div></div>";
-
-                    return markup;
-                }
-
-                function formatRepoSelection(repo) {
-                    return repo.full_name || repo.text;
-                }
-
-                $(".js-data-example-ajax").select2({
-                    width: "off",
-                    ajax: {
-                        url: "https://api.github.com/search/repositories",
-                        dataType: 'json',
-                        delay: 250,
-                        data: function (params) {
-                            return {
-                                q: params.term, // search term
-                                page: params.page
-                            };
-                        },
-                        processResults: function (data, page) {
-                            // parse the results into the format expected by Select2.
-                            // since we are using custom formatting functions we do not need to
-                            // alter the remote JSON data
-                            return {
-                                results: data.items
-                            };
-                        },
-                        cache: true
-                    },
-                    escapeMarkup: function (markup) {
-                        return markup;
-                    }, // let our custom formatter work
-                    minimumInputLength: 1,
-                    templateResult: formatRepo,
-                    templateSelection: formatRepoSelection
-                });
-
-                $("button[data-select2-open]").click(function () {
-                    $("#" + $(this).data("select2-open")).select2("open");
-                });
-
-                $(":checkbox").on("click", function () {
-                    $(this).parent().nextAll("select").prop("disabled", !this.checked);
-                });
-
-                // copy Bootstrap validation states to Select2 dropdown
-                //
-                // add .has-waring, .has-error, .has-succes to the Select2 dropdown
-                // (was #select2-drop in Select2 v3.x, in Select2 v4 can be selected via
-                // body > .select2-container) if _any_ of the opened Select2's parents
-                // has one of these forementioned classes (YUCK! ;-))
-                $(".select2, .select2-multiple, .select2-allow-clear, .js-data-example-ajax").on("select2:open", function () {
-                    if ($(this).parents("[class*='has-']").length) {
-                        var classNames = $(this).parents("[class*='has-']")[0].className.split(/\s+/);
-
-                        for (var i = 0; i < classNames.length; ++i) {
-                            if (classNames[i].match("has-")) {
-                                $("body > .select2-container").addClass(classNames[i]);
-                            }
-                        }
-                    }
-                });
-
-                $(".js-btn-set-scaling-classes").on("click", function () {
-                    $("#select2-multiple-input-sm, #select2-single-input-sm").next(".select2-container--bootstrap").addClass("input-sm");
-                    $("#select2-multiple-input-lg, #select2-single-input-lg").next(".select2-container--bootstrap").addClass("input-lg");
-                    $(this).removeClass("btn-primary btn-outline").prop("disabled", true);
-                });
-            };
-
-            return {
-                //main function to initiate the module
-                init: function () {
-                    handleDemo();
-                }
-            };
-
-        }();
-
-        if (App.isAngularJsApp() === false) {
-            jQuery(document).ready(function () {
-                ComponentsSelect2.init();
-            });
-        }
-        function showHiw(id) {
-//            $(".how-it-work").hide();
-//            $("#"+id).show();
-        }
 
     </script>
 @endsection
